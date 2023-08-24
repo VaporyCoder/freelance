cedbb339b0c003f7ef40ce9e31231c674a9970bf
Jon <jlara2019@gmail.comgit config --global user.email jlara2019@gmail.compwdgit commit>
2023-08-24 13:20:54 -0500

    added freelancers

---
+++ script.js
@@ -0,0 +1,11 @@
+const names = ['John Smith', 'Jake Johnson', 'John Doe', 'Alex Park', 'Peter Jackson', 'Harry Potter', 'Barry Allen'];
+const occupations = ['programmer', 'wizard', 'chef', 'banker', 'driver', 'trainer'];
+const freelancers = [
+ {name: 'John Smith', price: 40, occupation: 'banker'},
+ {name: 'Jake Johnson', price: 25, occupation: 'driver'},
+ {name: 'John Doe', price: 75, occupation: 'chef'},
+ {name: 'Alex Park', price: 65, occupation: 'programmer'},
+ {name: 'Peter Jackson', price: 60, occupation: 'programmer'},
+ {name: 'Harry Potter', price: 100, occupation: 'wizard'},
+ {name: 'Barry Allen', price: 100, occupation: 'trainer'}
+];
